9bcdd3be0b424f25b06d62536e759fe5565d21a1
Corals <77341395+Coralise@users.noreply.github.com>
2025-02-24 18:06:34 +0800

    Removed rate limiting for now

--- src/app/api/word/random/route.ts
+++ src/app/api/word/random/route.ts
@@ -1,27 +1,8 @@
 import { NextRequest } from "next/server";
 import { RandomWordRequestBody } from "../../../../../types/global";
 import fetchData from "../../../../../data/fetcher";
-import { LRUCache } from "lru-cache";
-
-const rateLimit = new LRUCache<string, { count: number; lastRequest: number }>({
-  max: 500, // Store up to 500 unique IPs
-  ttl: 60 * 1000, // Reset counts every 60 seconds
-});
-
-const MAX_REQUESTS = 30; // Allow 30 requests per minute
 
 export async function POST(req: NextRequest) {
-    const ip = req.headers.get("x-forwarded-for") ?? "unknown";
-  
-    const requestInfo = rateLimit.get(ip) || { count: 0, lastRequest: Date.now() };
-  
-    if (requestInfo.count >= MAX_REQUESTS) {
-      return Response.json({ error: "Too many requests, slow down!" }, { status: 429 });
-    }
-  
-    // Increment request count
-    requestInfo.count++;
-    rateLimit.set(ip, requestInfo);
     
     const body: RandomWordRequestBody = await req.json();
 
